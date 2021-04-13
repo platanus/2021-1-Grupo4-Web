6b4c52e747435eff3b2f55571e58d5f013abb3aa
pipe <fncabrera@uc.cl>
2021-04-12 21:53:34 -0400

    feat(@super-kitchen): setup ready :rocket:

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  scope path: '/api' do
+    api_version(module: 'Api::V1', path: { value: 'v1' }, defaults: { format: 'json' }) do
+    end
+  end
+  mount Rswag::Api::Engine => '/api-docs'
+  mount Rswag::Ui::Engine => '/api-docs'
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  devise_for :users
+  mount Sidekiq::Web => '/queue'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
