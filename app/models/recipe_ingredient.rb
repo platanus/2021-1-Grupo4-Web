501a06b4ba24b8fe5345f455d2ac3131ed4b338a
miortiz1 <miortiz1@uc.cl>
2021-04-16 18:09:25 -0400

    feat(crud): create models Recipe RecipeIngredients Menu

---
+++ app/models/recipe_ingredient.rb
@@ -0,0 +1,16 @@
+class RecipeIngredient < ApplicationRecord
+	belongs_to :ingredient
+	belongs_to :recipe
+end
+
+# == Schema Information
+#
+# Table name: recipe_ingredients
+#
+#  id            :bigint(8)        not null, primary key
+#  recipe_id     :integer
+#  ingredient_id :integer
+#  quantity      :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
