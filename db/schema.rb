252a6dceaf0255344294de2fad4d503b8f3ac31c
fede5 <fede@dealingapp.com>
2021-04-16 18:06:35 -0400

    feat(crud): model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_13_012800) do
+ActiveRecord::Schema.define(version: 2021_04_16_213909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,17 @@ ActiveRecord::Schema.define(version: 2021_04_13_012800) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.string "SKU"
+    t.integer "price"
+    t.string "currency"
+    t.integer "quantity"
+    t.string "measure"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
---
+++ spec/factories/ingredients.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :ingredient do
+    name { "MyString" }
+    SKU { "MyString" }
+    price { 1 }
+    currency { "MyString" }
+    quantity { 1 }
+    measure { "MyString" }
+  end
+end
