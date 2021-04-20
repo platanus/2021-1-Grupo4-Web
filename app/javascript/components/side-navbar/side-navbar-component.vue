1f1f483c8bd9b0ef80a72967e8f89ec762c4ab44
Sofia Delano <sofiadelano@MacBook-Air-de-Sofia.local>
2021-04-20 16:55:41 -0400

    feat(SideNavbar): add side navbar

---
+++ app/javascript/components/side-navbar/side-navbar-component.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex flex-col items-start pt-0 pl-0 pr-0 pb-8 absolute w-64 h-screen left-0 top-16 bg-gray-800">
+    <item />
+    <item />
+    <item />
+  </div>
+</template>
+
+<script>
+
+import Item from './side-navbar-item-component.vue';
+
+export default {
+  components: {
+    Item,
+  },
+  setup() {
+    return (null);
+  },
+};
+</script>
---
+++ app/javascript/components/side-navbar/side-navbar-label-component.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="static w-16 h-0.5 left-16 top-4 not-italic font-normal text-xl text-white">
+    hola
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return (null);
+  },
+};
+</script>
