db898bb038bb0773712a1236f33494d74c649728
fede5 <fede@dealingapp.com>
2021-04-20 10:33:37 -0400

    refactor(cruds): delete not used files

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -7,10 +7,10 @@ end
 #
 # Table name: recipe_ingredients
 #
-#  id                  :bigint(8)        not null, primary key
-#  recipe_id           :integer
-#  ingredient_id       :integer
-#  ingredient_quantity :integer
-#  created_at          :datetime         not null
-#  updated_at          :datetime         not null
+#  id            :bigint(8)        not null, primary key
+#  recipe_id     :integer
+#  ingredient_id :integer
+#  quantity      :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
 #
