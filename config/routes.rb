984f6bd27f861da1937a38bbe0e10ca6f4570caf
fede5 <fede@dealingapp.com>
2021-04-20 11:16:05 -0400

    refactor(cruds): change ruby version to 2.7 and delete empty spaces

--- config/routes.rb
+++ config/routes.rb
@@ -15,9 +15,7 @@ Rails.application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-
   devise_for :users
-  mount Sidekiq::Web => '/queue'
-  
+  mount Sidekiq::Web => '/queue'  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
