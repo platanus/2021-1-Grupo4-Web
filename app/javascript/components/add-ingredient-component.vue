815236d1616982f7a251b5fc882595dcfa50f894
ValentinaAlvarezG <vjalvarez@uc.cl>
2021-04-21 14:00:22 -0400

    fix(modal-add-ingredient): allow modal to close

--- app/javascript/components/add-ingredient-component.vue
+++ app/javascript/components/add-ingredient-component.vue
@@ -1,5 +1,4 @@
 <template>
-
 <div class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
   <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
     <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
@@ -42,6 +41,7 @@
   </div>
 </div>
 
+
 </template>
 
 <script>
@@ -59,6 +59,7 @@ methods:{
 
     cancel(){
         console.log("Cancelar");
+        this.$emit('cancel');
     },
 }
 }
