1454db53f9ecb169dd0eccbf66cdc49cc67cfa71
ValentinaAlvarezG <vjalvarez@uc.cl>
2021-04-20 22:14:08 -0400

    refactor(side-navbar): change sidenavbar to look better

--- app/javascript/components/top-navbar-component.vue
+++ app/javascript/components/top-navbar-component.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="bg-gray-800">
+    <nav class="bg-black">
         <div class="max-w-7xl px-2 sm:px-6 lg:px-8">
             <div class="relative flex items-center justify-between h-16">
                 <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
