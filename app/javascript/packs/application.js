9706079a8bb245c4f28356498244ab15b52965a2
ValentinaAlvarezG <vjalvarez@uc.cl>
2021-04-20 21:34:20 -0400

    feat(top-navbar): add top-navbar to layout

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,14 +1,14 @@
 import Vue from 'vue/dist/vue.esm';
 import App from '../app.vue';
 import '../css/application.css';
-import Navbar from '../components/top-navbar-component.vue';
+import TopNavbar from '../components/top-navbar-component.vue';
 import Ingredients from '../components/ingredients-component.vue';
 import SideNavbar from '../components/side-navbar/side-navbar-component.vue';
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
     el: '#vue-app',
-    components: { App, Navbar, Ingredients, SideNavbar },
+    components: { App, TopNavbar, Ingredients, SideNavbar },
   });
 
   return app;
