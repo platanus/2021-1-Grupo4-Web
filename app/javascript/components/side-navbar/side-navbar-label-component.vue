6480d614b3d9c4a0e8473d44975e43a6829a6462
ValentinaAlvarezG <vjalvarez@uc.cl>
2021-04-21 00:26:37 -0400

    refactor(side-navbar-props): add props to side-navbar

--- app/javascript/components/side-navbar/side-navbar-label-component.vue
+++ app/javascript/components/side-navbar/side-navbar-label-component.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="static w-16 pl-2 items-center left-16 not-italic font-normal text-xl text-white">
-    Label
+    {{ label }}
   </div>
 </template>
 
 <script>
 export default {
+  props: {
+    label: { type: String, required: true},
+    },
+
   setup() {
     return (null);
   },
