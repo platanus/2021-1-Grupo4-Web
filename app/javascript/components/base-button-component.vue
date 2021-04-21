748119c2c60c94fc93669f672e9fe7894d427e61
ValentinaAlvarezG <vjalvarez@uc.cl>
2021-04-21 13:19:32 -0400

    feat(modal-add-ingredient): add modal when click button

--- app/javascript/components/base-button-component.vue
+++ app/javascript/components/base-button-component.vue
@@ -1,6 +1,10 @@
 <template>
-    <button @click="clickButton" class="mx-2 my-2 h-10 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-      {{ elements.placeholder }}
+    <button  
+      class="mx-2 my-2 h-10 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+      :class="elements.color"
+      @click="clickButton"
+    >
+        {{ elements.placeholder }}
     </button>
 </template>
 
