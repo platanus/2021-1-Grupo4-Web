87cefc815543940ddd91df20dddb10d6e3f6a560
fede5 <fede@dealingapp.com>
2021-04-21 17:01:46 -0400

    test(cruds): add factory attributes

--- spec/factories/recipe_ingredients.rb
+++ spec/factories/recipe_ingredients.rb
@@ -1,13 +1,7 @@
 FactoryBot.define do
   factory :recipe_ingredient do
-<<<<<<< HEAD
     recipe_id { 1 }
     ingredient_id { 1 }
     quantity { 1 }
-=======
-    recipe_id { "" }
-    ingredient_id { "" }
-    quantity { "" }
->>>>>>> ce2d487 (feat(crud): create models Recipe RecipeIngredients Menu)
   end
 end
