8d34a4bb8167f9f01a996f4e7c105703353c4f01
Luciano Davico <lldavico@uc.cl>
2021-04-22 14:03:54 -0400

    feat(lider-scraper): add lider scraper for products and headless Chrome

--- app/clients/chrome_client.rb
+++ app/clients/chrome_client.rb
@@ -1,8 +1,20 @@
+require 'selenium-webdriver'
+
 class ChromeClient
   CHROME_CLIENT_TIMEOUT = 35
+  ENABLE_HEADLESS_CHROME = true
+
+  @@options = Selenium::WebDriver::Chrome::Options.new
 
   def browser
-    @browser ||= Pincers.for_webdriver(:chrome, wait_timeout: CHROME_CLIENT_TIMEOUT)
+    if ENABLE_HEADLESS_CHROME
+      set_headless_chrome
+    end
+    @browser ||= Pincers.for_webdriver(:chrome, wait_timeout: CHROME_CLIENT_TIMEOUT, options: @@options)
+  end
+
+  def set_headless_chrome
+    @@options.add_argument('--headless')
   end
 
   def ensuring_browser_closure(&block)
