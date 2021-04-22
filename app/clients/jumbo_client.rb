4c487392652c3c0d03fa37d6239df9da9b0a5a91
Caco <jiossandon@uc.cl>
2021-04-22 19:32:39 -0400

    fix(jumbo-scrapper): use old prices if normal doesnt exist

--- app/clients/jumbo_client.rb
+++ app/clients/jumbo_client.rb
@@ -1,5 +1,5 @@
 class JumboClient < ChromeClient
-  BASE_URL = 'https://www.jumbo.cl/busqueda?ft='
+  BASE_URL = 'https://www.jumbo.cl/busqueda'
 
   def products_by_query(query: nil)
     ensuring_browser_closure do
@@ -7,7 +7,7 @@ class JumboClient < ChromeClient
 
       browser.search('.shelf-item').wait(:present, timeout: 5.0).map do |product|
         {
-          price: get_price(product),
+          price: get_price(product) || get_old_price(product) || get_offer_price(product),
           measure: get_measure(product),
           name: get_name(product)
         }
@@ -18,18 +18,35 @@ class JumboClient < ChromeClient
   private
 
   def get_price(product)
-    product.search('.product-single-price-container').text.split('\n').first
+    price = product.search('.product-single-price-container').text.split("\n").first
+    format_search(price)
+  end
+
+  def get_old_price(product)
+    price = product.search('.price-product-value').text
+    format_search(price)
+  end
+
+  def get_offer_price(product)
+    price = product.search('.price-product-best').first.text
+    format_search(price)
   end
 
   def get_measure(product)
-    product.search('.shelf-single-unit').text
+    measure = product.search('.shelf-single-unit').text
+    format_search(measure)
   end
 
   def get_name(product)
-    product.search('.shelf-product-title h2').first.text
+    name = product.search('.shelf-product-title h2').first.text
+    format_search(name)
+  end
+
+  def format_search(result)
+    result.presence ? result : nil
   end
 
   def go_to_products_page(query)
-    browser.goto(BASE_URL + query)
+    browser.goto "#{BASE_URL}?ft=#{query}"
   end
 end
