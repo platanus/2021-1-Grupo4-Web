4e72425f597cf54f26cc4b05263d06c6678153df
Andrés Rodríguez <arodriguez14@uc.cl>
2021-04-22 16:33:56 -0300

    feat(crud): Test procider ready

--- spec/models/provider_spec.rb
+++ spec/models/provider_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Provider, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:subject) { create(:provider) }
+  describe 'it has all the attributes' do
+    it "should include the :id attribute" do
+      expect(subject.attributes).to include("id")
+    end
+    it "should include the :name attribute" do
+      expect(subject.attributes).to include("name")
+    end
+    it "should include the :address attribute" do
+      expect(subject.attributes).to include("address")
+    end
+    it "should include the :country attribute" do
+      expect(subject.attributes).to include("country")
+    end
+  end
+  describe 'it has many ingredients' do
+    it { should have_many(:ingredients) }
+  end
 end
