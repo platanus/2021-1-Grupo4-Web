60b8d9104cd81ffc66b16ec3f4776c1d2d52898a
aescobar4 <aescobar@uc.cl>
2021-04-22 15:38:59 -0400

    style(ingredients-sidenavbar): change color of side navbar

--- app/javascript/components/side-navbar/side-navbar-component.vue
+++ app/javascript/components/side-navbar/side-navbar-component.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="flex flex-col items-start pt-0 pl-0 pr-0 pb-8 absolute w-64 h-screen left-0 top-16 bg-black">
-    <div v-for="option in options">
-      <item :option="option" />
+  <div class="flex flex-col items-start pt-0 pl-0 pr-0 pb-8 absolute w-64 h-screen left-0 top-16 bg-gray-800">
+    <div v-for="option in options.elements">
+     <item v-if="options.active_element===option" :option="option" :active="true" />
+      <item v-else :option="option" :active="false" />
     </div>
   </div>
 </template>
@@ -16,7 +17,7 @@ export default {
   },
 
   props: {
-    options: {type: Array, required: true},
+    options: {type: Object, required: true},
   },
 
   setup() {
