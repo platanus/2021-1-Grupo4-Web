e1eabe854f6e43cb056628fdc9a24cf30d930788
pipe <fncabrera@uc.cl>
2021-04-24 13:11:48 -0400

    refactor(scrappers): rubocop warnings fixed

--- app/clients/chrome_client.rb
+++ app/clients/chrome_client.rb
@@ -10,7 +10,8 @@ class ChromeClient
     if ENABLE_HEADLESS_CHROME
       set_headless_chrome
     end
-    @browser ||= Pincers.for_webdriver(:chrome, wait_timeout: CHROME_CLIENT_TIMEOUT, options: @@options)
+    @browser ||= Pincers.for_webdriver(:chrome, wait_timeout: CHROME_CLIENT_TIMEOUT,
+                                                options: @@options)
   end
 
   def set_headless_chrome
