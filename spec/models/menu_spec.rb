34f4cab84c2fc5a8734263797c43ab82eec114a6
Andrés Rodríguez <arodriguez14@uc.cl>
2021-04-25 13:08:10 -0300

    feat(crud): test models menu, menu_recipe and recipe_ingredient

--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Menu, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe Menu do
+    subject(:menu) { build(:menu) }
+    it { is_expected.to belong_to(:user) }
+  end
+  describe 'it has all the attributes' do
+    it "includes the :id attribute" do
+      expect(subject.attributes).to include("id")
+    end
+    it "includes the :name attribute" do
+      expect(subject.attributes).to include("name")
+    end
+  end
+  describe 'it has many recipes' do
+    it { is_expected.to have_many(:menu_recipes) }
+  end
+  describe Menu do
+    it { should have_many(:recipes).through(:menu_recipes) }
+  end
 end
--- spec/models/recipe_ingredient_spec.rb
+++ spec/models/recipe_ingredient_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe RecipeIngredient, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe RecipeIngredient do
+    subject(:recipe_ingredient) { build(:recipe_ingredient) }
+    it { is_expected.to belong_to(:recipe) }
+  end
+  describe RecipeIngredient do
+    subject(:recipe_ingredient) { build(:recipe_ingredient) }
+    it { is_expected.to belong_to(:ingredient) }
+  end
+  describe 'it has all the attributes' do
+    it "includes the :id attribute" do
+      expect(subject.attributes).to include("id")
+    end
+    it "includes the :ingredient_quantity attribute" do
+      expect(subject.attributes).to include("ingredient_quantity")
+    end
+  end
 end
