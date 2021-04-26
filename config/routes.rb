a3a4213336da44fa9b3d0493bec925dc967116f7
fthurtado <fthurtado@uc.cl>
2021-04-26 19:19:59 -0400

    fix(): routes error

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
         devise_scope :user do
           resources :registrations, only: [:create]
           resources :sessions, only: [:create]
-          resources :ingredientss, only: [:index, :create]
+          resources :ingredients, only: [:index, :create]
         end
       end
 
