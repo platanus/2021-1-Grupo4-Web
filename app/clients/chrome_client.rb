9392ff13646efa1ff0574657e13d7a2cd9656d1b
Caco <jiossandon@uc.cl>
2021-04-27 20:40:11 -0400

    fix(scraper@chrome): error without chrome env variables

--- app/clients/chrome_client.rb
+++ app/clients/chrome_client.rb
@@ -18,24 +18,22 @@ class ChromeClient
   private
 
   def driver
-    chrome_path = ENV.fetch('GOOGLE_CHROME_BIN_PATH', nil)
-    return Selenium::WebDriver.for(:chrome, options: chrome_options) unless chrome_path
-
+    chrome_path = ENV.fetch('GOOGLE_CHROME_BIN_PATH')
     set_chromedriver_path
+
     Selenium::WebDriver.for(:chrome, options: chrome_options(chrome_path))
   end
 
   def set_chromedriver_path
-    chromedriver_path = ENV.fetch('GOOGLE_CHROMEDRIVER_BIN_PATH', nil)
-    return unless chromedriver_path
+    chromedriver_path = ENV.fetch('GOOGLE_CHROMEDRIVER_BIN_PATH')
 
     Selenium::WebDriver::Chrome::Service.driver_path = chromedriver_path
   end
 
-  def chrome_options(chrome_path = nil)
+  def chrome_options(chrome_path)
     chrome_options = Selenium::WebDriver::Chrome::Options.new
     chrome_options.add_argument("--headless")
-    chrome_options.binary = chrome_path if chrome_path.present?
+    chrome_options.binary = chrome_path
 
     chrome_options
   end
