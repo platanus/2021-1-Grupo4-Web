ed3dc60be539462a322ab6978b1a72e72c301b56
Luciano Davico <lldavico@uc.cl>
2021-04-28 12:17:31 -0400

    fix(app@chrome-client): fix jumbo client conflicts

--- app/clients/jumbo_client.rb
+++ app/clients/jumbo_client.rb
@@ -51,10 +51,6 @@ class JumboClient < ChromeClient
   end
 
   def go_to_products_page(query)
-<<<<<<< HEAD
     browser.goto "#{BASE_URL}?ft=#{query}"
-=======
-    browser.goto("#{BASE_URL}?ft=#{query}")
->>>>>>> ef8f99f... BASE[#12] style(superkitchen@scrapers): fix cops and some code issues
   end
 end
