f097ed72de266a1d6a171f5eb3d67550ac9df9b0
ValentinaAlvarezG <vjalvarez@uc.cl>
2021-05-01 20:55:46 -0400

    feat(navbar-login-links): add to nabvar links to register and login

--- app/javascript/components/layout/top-navbar-component.vue
+++ app/javascript/components/layout/top-navbar-component.vue
@@ -1,16 +1,20 @@
 <template>
-  <nav class="bg-gray-800">
-    <div class="max-w-7xl px-2 sm:px-6 lg:px-8">
-      <div class="relative flex items-center justify-between h-16">
-        <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-          <div class="flex-shrink-0 flex items-center">
-            <a
-              href="/"
-              class="text-white px-3 py-2 rounded-md text-lg font-bold"
-            >KitchenGram</a>
-          </div>
-        </div>
-      </div>
+  <nav class="flex items-center justify-between bg-gray-800 p-6 h-16">
+    <div class="flex items-center flex-shrink-0 text-white mr-6">
+      <a
+        href="/"
+        class="font-semibold text-xl tracking-widest"
+      >KitchenGram</a>
+    </div>
+    <div class="w-full flex-grow lg:flex lg:items-end lg:w-auto justify-end pr-5">
+      <a
+        href="/users/sign_in"
+        class="text-lg px-4 py-2 text-white hover:bg-gray-900 mt-4 lg:mt-0"
+      >{{ $t('msg.users.login') }}</a>
+      <a
+        href="/users/sign_up"
+        class="text-lg px-4 py-2 text-white hover:bg-gray-900 mt-4 lg:mt-0"
+      >{{ $t('msg.users.register') }}</a>
     </div>
   </nav>
 </template>
