75c1d6c23ed5a3658086c852e4cb0ef7ae5c771e
aescobar4 <aescobar4@uc.cl>
2021-05-05 10:08:09 -0400

    feat(frontend/create-recipe): new component

--- app/javascript/components/recipes/create/container-component.vue
+++ app/javascript/components/recipes/create/container-component.vue
@@ -1,11 +1,24 @@
 <template>
-    <div>
-        
-    </div>
+  <div>
+    <img 
+      
+    >
+    <input-form
+      v-if="input"
+      id="grams"
+      type="text"
+      placeholder="Hay que arreglar el value"
+      value=""
+    />
+  </div>
 </template>
 
 <script>
 export default {
-    
+  props: {
+    key: { type: String, required: true },
+    input: { type: Boolean, required: true },
+    svg: { type: Object, required: true },
+  },
 }
 </script>
