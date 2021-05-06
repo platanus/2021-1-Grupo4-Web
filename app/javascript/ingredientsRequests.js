a70f52ef8447c5bb9159e608345bd686dca47514
pipe <fncabrera@uc.cl>
2021-05-05 20:25:28 -0400

    feat(ingredients): conection to backend and methods for api in aux file

---
+++ app/javascript/ingredientsRequests.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+async function getIngredients(email, token) {
+  return (axios
+  .get('https://pl-super-kitchen-staging.herokuapp.com/api/v1/ingredients', { 
+    params: {
+    'user_email': email,
+    'user_token': token,
+    } 
+  }));
+};
+
+async function postIngredient(newIngredient, email, token) {
+  return (axios
+          .post('https://pl-super-kitchen-staging.herokuapp.com/api/v1/ingredients', {
+            ingredient: {
+              'provider_id': newIngredient.providerId,
+              'name': newIngredient.name,
+              'sku': newIngredient.sku,
+              'price': newIngredient.price,
+              'currency': newIngredient.currency,
+              'quantity': newIngredient.quantity,
+              'measure': newIngredient.measure,
+            },
+          }, { params: {
+            'user_email': email,
+            'user_token': token,
+          } }));
+};
+
+export { getIngredients, postIngredient };
