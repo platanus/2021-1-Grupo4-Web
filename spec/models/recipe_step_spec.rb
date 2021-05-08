16c0a819a0025c58859df37349a5616530e65d72
cacosandon <jiossandon@uc.cl>
2021-05-07 21:16:45 -0400

    style(specs): move from clause to newline

--- spec/models/recipe_step_spec.rb
+++ spec/models/recipe_step_spec.rb
@@ -54,59 +54,81 @@ RSpec.describe RecipeStep, type: :model do
       ).to have_attributes(step_order_rank: 4)
     end
 
-    # rubocop:disable Layout/MultilineMethodCallIndentation
     it 'moves forward only first and second when moving third backwards' do
       expect { bake.update(step_order_position: 0) }
-        .to change { prepare.reload.step_order_rank }.from(0).to(1)
-        .and change { cook.reload.step_order_rank }.from(1).to(2)
-        .and change { decorate.reload.step_order_rank }.by(0)
+        .to change { prepare.reload.step_order_rank }
+        .from(0).to(1)
+        .and change { cook.reload.step_order_rank }
+        .from(1).to(2)
+        .and change { decorate.reload.step_order_rank }
+        .by(0)
     end
 
     it 'moves backward only third and fourth when moving second forward' do
       expect { cook.update(step_order_position: 3) }
-        .to change { bake.reload.step_order_rank }.from(2).to(1)
-        .and change { decorate.reload.step_order_rank }.from(3).to(2)
-        .and change { prepare.reload.step_order_rank }.by(0)
+        .to change { bake.reload.step_order_rank }
+        .from(2).to(1)
+        .and change { decorate.reload.step_order_rank }
+        .from(3).to(2)
+        .and change { prepare.reload.step_order_rank }
+        .by(0)
     end
 
     it 'make an exchange of positions when are consecutive on backwards' do
       expect { decorate.update(step_order_position: 2) }
-        .to change { bake.reload.step_order_rank }.from(2).to(3)
-        .and change { decorate.reload.step_order_rank }.from(3).to(2)
-        .and change { cook.reload.step_order_rank }.by(0)
+        .to change { bake.reload.step_order_rank }
+        .from(2).to(3)
+        .and change { decorate.reload.step_order_rank }
+        .from(3).to(2)
+        .and change { cook.reload.step_order_rank }
+        .by(0)
     end
 
     it 'make an exchange of positions when are consecutive on forward' do
       expect { prepare.update(step_order_position: 1) }
-        .to change { cook.reload.step_order_rank }.from(1).to(0)
-        .and change { prepare.reload.step_order_rank }.from(0).to(1)
-        .and change { bake.reload.step_order_rank }.by(0)
+        .to change { cook.reload.step_order_rank }
+        .from(1).to(0)
+        .and change { prepare.reload.step_order_rank }
+        .from(0).to(1)
+        .and change { bake.reload.step_order_rank }
+        .by(0)
     end
 
     it 'moves all forward when the last move to the first position' do
       expect { decorate.update(step_order_position: 0) }
-        .to change { decorate.reload.step_order_rank }.from(3).to(0)
-        .and change { bake.reload.step_order_rank }.from(2).to(3)
-        .and change { cook.reload.step_order_rank }.from(1).to(2)
-        .and change { prepare.reload.step_order_rank }.from(0).to(1)
+        .to change { decorate.reload.step_order_rank }
+        .from(3).to(0)
+        .and change { bake.reload.step_order_rank }
+        .from(2).to(3)
+        .and change { cook.reload.step_order_rank }
+        .from(1).to(2)
+        .and change { prepare.reload.step_order_rank }
+        .from(0).to(1)
     end
 
     it 'moves all backward when the fist move to the last position' do
       expect { prepare.update(step_order_position: 3) }
-        .to change { decorate.reload.step_order_rank }.from(3).to(2)
-        .and change { bake.reload.step_order_rank }.from(2).to(1)
-        .and change { cook.reload.step_order_rank }.from(1).to(0)
-        .and change { prepare.reload.step_order_rank }.from(0).to(3)
+        .to change { decorate.reload.step_order_rank }
+        .from(3).to(2)
+        .and change { bake.reload.step_order_rank }
+        .from(2).to(1)
+        .and change { cook.reload.step_order_rank }
+        .from(1).to(0)
+        .and change { prepare.reload.step_order_rank }
+        .from(0).to(3)
     end
 
     it 'does not move anything if change of step description' do
       expect { prepare.update(description: 'Era sal, no azúcar jaja') }
-        .to change { decorate.reload.step_order_rank }.by(0)
-        .and change { bake.reload.step_order_rank }.by(0)
-        .and change { cook.reload.step_order_rank }.by(0)
-        .and change { prepare.reload.step_order_rank }.by(0)
+        .to change { decorate.reload.step_order_rank }
+        .by(0)
+        .and change { bake.reload.step_order_rank }
+        .by(0)
+        .and change { cook.reload.step_order_rank }
+        .by(0)
+        .and change { prepare.reload.step_order_rank }
+        .by(0)
     end
-    # rubocop:enable Layout/MultilineMethodCallIndentation
   end
 
   describe 'deleting step' do
@@ -123,26 +145,32 @@ RSpec.describe RecipeStep, type: :model do
       create(:recipe_step, step_order_position: 3, description: 'A decorar!', recipe: recipe)
     end
 
-    # rubocop:disable Layout/MultilineMethodCallIndentation
     it 'reduces by 1 all when first is deleted' do
       expect { prepare.destroy! }
-        .to change { cook.reload.step_order_rank }.from(1).to(0)
-        .and change { bake.reload.step_order_rank }.from(2).to(1)
-        .and change { decorate.reload.step_order_rank }.from(3).to(2)
+        .to change { cook.reload.step_order_rank }
+        .from(1).to(0)
+        .and change { bake.reload.step_order_rank }
+        .from(2).to(1)
+        .and change { decorate.reload.step_order_rank }
+        .from(3).to(2)
     end
 
     it 'moves backward only fourth' do
       expect { bake.destroy! }
-        .to change { decorate.reload.step_order_rank }.from(3).to(2)
-        .and change { cook.reload.step_order_rank }.by(0)
+        .to change { decorate.reload.step_order_rank }
+        .from(3).to(2)
+        .and change { cook.reload.step_order_rank }
+        .by(0)
     end
 
     it 'no step_order_rank is reduced because of tail destroy' do
       expect { decorate.destroy! }
-        .to change { prepare.reload.step_order_rank }.by(0)
-        .and change { cook.reload.step_order_rank }.by(0)
-        .and change { bake.reload.step_order_rank }.by(0)
+        .to change { prepare.reload.step_order_rank }
+        .by(0)
+        .and change { cook.reload.step_order_rank }
+        .by(0)
+        .and change { bake.reload.step_order_rank }
+        .by(0)
     end
-    # rubocop:enable Layout/MultilineMethodCallIndentation
   end
 end
