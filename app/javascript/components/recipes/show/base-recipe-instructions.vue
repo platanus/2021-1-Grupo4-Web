37f1f247b1a5dc88fcfad42ed4bcbdf52d44e12c
sofiadelano <smdelano@uc.cl>
2021-05-08 14:51:38 -0400

    fix(recipes/show): change felx-grow-1 for flex-grow

--- app/javascript/components/recipes/show/base-recipe-instructions.vue
+++ app/javascript/components/recipes/show/base-recipe-instructions.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col items-start pt-4 w-5/6 h-auto flex-grow-1 ml-6">
+  <div class="flex flex-col items-start pt-4 w-5/6 h-auto flex-grow ml-6">
     <div class="w-13 h-6 font-sans font-normal text-lg text-black flex-grow-0 mt-2.5">
       {{ $t('msg.recipes.steps') }}
     </div>
