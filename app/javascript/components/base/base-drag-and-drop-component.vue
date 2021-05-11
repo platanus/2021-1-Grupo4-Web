6b6bd88c22bb0657a0a1daf69a6b63d61bc20077
aescobar4 <aescobar4@uc.cl>
2021-05-10 21:21:39 -0400

    feat(frontend/crd/create-recipe): new feats

---
+++ app/javascript/components/base/base-drag-and-drop-component.vue
@@ -0,0 +1,52 @@
+<template>
+  <div
+    id="app"
+    class="min-h-screen w-screen bg-gray-200 flex items-center justify-center"
+  >
+    <draggable
+      :list="elements"
+      :animation="200"
+      ghost-class="moving-card"
+      group="steps"
+      filter=".action-button"
+      class="w-full max-w-md"
+      tag="ul"
+    >
+      <container
+        v-for="element in elements"
+        :key="element.id"
+        :element="element"
+        :svg="svg"
+        :input="input"
+        :color="color()"
+      />
+    </draggable>
+  </div>
+</template>
+
+<script>
+import draggable from 'vuedraggable';
+
+export default {
+  props: {
+    elements: { type: Array, required: true },
+  },
+  components: {
+    draggable,
+  },
+  data() {
+    return {
+      enabled: true,
+      dragging: false,
+    };
+  },
+  methods: {
+    onEdit(user) {
+      console.log(`Editing ${user.name}`);
+    },
+    onDelete(user) {
+      console.log(`Deleting ${user.name}`);
+    },
+  },
+};
+</script>
---
+++ app/javascript/components/base/base-text-box-component.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <textarea
+      :placeholder="placeholder"
+      class="resize-y border-2 rounded-md border-gray-300 w-full my-4 p-4"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    placeholder: { type: String, required: true },
+  },
+};
+</script>
