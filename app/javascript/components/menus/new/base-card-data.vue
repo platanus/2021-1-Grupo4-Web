0046644ec9d9606be023bbee9629881b8c5fa37c
sofiadelano <smdelano@uc.cl>
2021-05-12 12:54:12 -0400

    fix(menus/create): change recipes selected and add total price of menu

--- app/javascript/components/menus/new/base-card-data.vue
+++ app/javascript/components/menus/new/base-card-data.vue
@@ -22,12 +22,12 @@
         <div class="w-3 h-3 flex-grow-0 mr-2">
           <img
             svg-inline
-            src="../../../../assets/images/magnifyng-glass-svg.svg"
+            src="../../../../assets/images/dolar-svg.svg"
             class="w-3 h-3 text-yellow-700"
           >
         </div>
         <div class="h-5 w-auto font-hind font-light text-sm text-black flex-grow-0">
-          3.000
+          $3.000 CLP
         </div>
       </div>
       <div class="flex items-center h-5 w-auto flex-grow-0">
--- app/javascript/components/menus/new/base-selected-recipes.vue
+++ app/javascript/components/menus/new/base-selected-recipes.vue
@@ -5,6 +5,9 @@
     <recipe-selected />
     <recipe-selected />
     <recipe-selected />
+    <div class="w-auto h-16 bg-white self-stretch flex-grow-0 py-5 text-right font-hind font-bold text-base text-black">
+      Total del menú: $25.000
+    </div>
   </div>
 </template>
 
