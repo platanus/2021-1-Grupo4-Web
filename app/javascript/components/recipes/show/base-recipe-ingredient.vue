36e5518a30e7025fd30db3bebe5a889f66555a33
sofiadelano <smdelano@uc.cl>
2021-05-13 16:18:31 -0400

    fix(recipe/ingredients): change style of recipes ingredients

--- app/javascript/components/recipes/show/base-recipe-ingredient.vue
+++ app/javascript/components/recipes/show/base-recipe-ingredient.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex justify-between items-start w-48 self-stretch flex-grow-0 my-3">
-    <div class="w-24 h-5 font-sans font-normal text-base text-black flex-grow-0">
+  <div class="flex justify-between items-center w-48 self-stretch flex-grow-0 my-3">
+    <div class="min-w-min h-5 font-sans font-normal text-base text-black flex-grow-0">
       {{ ingredient.name }}
     </div>
     <div class="w-9 h-5 font-sans font-normal text-base text-black flex-grow-0">
