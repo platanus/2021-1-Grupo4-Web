83b4647458fe7feec8ee887853dd1f89907b0c64
aescobar4 <aescobar4@uc.cl>
2021-05-16 22:27:49 -0400

    feat(frontend/create-recipe): everything ready but drag and drop

--- app/javascript/components/recipes/create/base-list-component.vue
+++ app/javascript/components/recipes/create/base-list-component.vue
@@ -12,14 +12,14 @@
           :text="element"
           :svg="svg"
           :input="input"
-          :color="color()"
+          :color="color('elements', element)"
         />
       </div>
     </div>
 
     <div v-if="input">
       <container
-        text="Costo total"
+        :text="$t('msg.recipes.cost')"
         :svg="{}"
         :input="false"
         :color="false"
@@ -28,8 +28,8 @@
 
       <base-button
         :elements="{
-          placeholder: 'Buscar mas ingredientes',
-          color: 'bg-white hover:bg-gray-300 text-black'
+          placeholder: $t('msg.recipes.more'),
+          color: 'bg-white text-black border-2 border-blue-600 align-center hover:bg-gray-300 h-12'
         }"
         @click="search"
         class="w-full"
@@ -39,22 +39,42 @@
     <div
       v-if="drag"
     >
-      <drag-and-drop
+      <!--drag-and-drop
         :elements="this.steps"
         :svg="svg"
-      />
+        :forceupdate="this.forceupdate"
+      /-->
+      <div
+        v-for="step in this.steps"
+        :key="step"
+      >
+        <container
+          :text="step"
+          :svg="svg"
+          :input="input"
+          :color="color('steps', step)"
+          @del="deleteStep"
+          @edit="editStep"
+        />
+      </div>
 
       <text-box
-        placeholder="Paso de la receta..."
+        :placeholder="$t('msg.recipes.step')"
       />
 
-      <base-button
-        :elements="{
-          placeholder: 'Agregar paso',
-          color: 'bg-white hover:bg-gray-300 text-black'
-        }"
-        @click="addStep"
-      />
+      <div class="grid grid-cols-4">
+        <div />
+        <div />
+        <div />
+        <base-button
+          :elements="{
+            placeholder: $t('msg.recipes.addstep'),
+            color: 'bg-white text-black border-2 border-blue-600 align-center hover:bg-gray-300 h-12'
+          }"
+          @click="addStep"
+          class="w-full"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -72,22 +92,45 @@ export default {
     return {
       par: false,
       steps: [],
+      forceupdate: false,
     };
   },
   methods: {
     search() {
       return null;
     },
-    color() {
-      this.par = !this.par;
+    color(list, element) {
+      if (list === 'elements') {
+        // eslint-disable-next-line no-magic-numbers
+        const index = this.elements.indexOf(element) % 2;
+        if (index === 0) {
+          return true;
+        }
+      } else {
+        // eslint-disable-next-line no-magic-numbers
+        const index = this.steps.indexOf(element) % 2;
+        if (index === 0) {
+          return true;
+        }
+      }
 
-      return this.par;
+      return false;
     },
     addStep() {
       const text = document.getElementById('textarea');
       this.steps.push(text.value);
-      console.log(this.steps);
       text.value = '';
+      this.forceUpdate = true;
+      this.$forceUpdate();
+    },
+    deleteStep(originalText) {
+      const index = this.steps.indexOf(originalText);
+      this.steps.splice(index, 1);
+      this.$forceUpdate();
+    },
+    editStep(originalText, newText) {
+      const index = this.steps.indexOf(originalText);
+      this.steps[index] = newText;
       this.$forceUpdate();
     },
   },
