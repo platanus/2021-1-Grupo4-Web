f1c45ac4770f9df5000d37b03834dbdc7f010540
ValentinaAlvarezG <vjalvarez@uc.cl>
2021-05-20 20:03:47 -0400

    refactor(components-names): modify name of components and import

--- app/javascript/components/menus/index/menus-table.vue
+++ app/javascript/components/menus/index/menus-table.vue
@@ -3,76 +3,46 @@
     <thead class="justify-between">
       <tr class="bg-yellow-500 border-4 border-yellow-500 py-1 text-left">
         <th
+          v-for="attribute in header"
+          :key="attribute"
           class="pl-2"
         >
-          <span class="text-white font-bold">{{ $t('msg.menus.name') }}</span>
+          <span class="text-white font-bold">{{ $t(`msg.${modelType}.${attribute}`) }}</span>
         </th>
-        <th
-          class="pl-2"
-        >
-          <span class="text-white font-bold">{{ $t('msg.menus.totalPrice') }}</span>
-        </th>
-        <th
-          class="pl-2"
-        >
-          <span class="text-white font-bold">{{ $t('msg.menus.recipesQuantity') }}</span>
-        </th>
-        <th
-          class="pl-2"
-        >
-          <span class="text-white font-bold">{{ $t('msg.menus.recipes') }}</span>
-        </th>
-        <th />
+        <th v-if="dots" />
       </tr>
     </thead>
     <tbody class="bg-gray-200">
       <tr
-        v-for="menu in menus"
-        :key="menu.id"
+        v-for="parsedElement in parsedElements"
+        :key="parsedElement.name"
         class="bg-white border-4 border-gray-200"
       >
-        <!-- name -->
         <td
-          key="name"
+          v-for="property in header"
+          :key="property"
           class="content-center py-2 pl-2"
         >
           <p
+            v-if="property!='recipes'"
             class="content-center font-semibold text-left"
           >
-            {{ menu.name }}
+            {{ parsedElement[property] }}
           </p>
-        </td>
-        <!-- totalPrice -->
-        <td
-          key="totalPrice"
-          class="content-center py-2 pl-2"
-        >
-          <p
-            class="content-center font-semibold text-left"
+          <div
+            v-else
+            class="flex flex-col"
           >
-            {{ 20.000 }}
-          </p>
+            <div
+              v-for="element in parsedElement[property]"
+              :key="element"
+            >
+              {{ element }}
+            </div>
+          </div>
         </td>
-        <!-- recipesQuantity -->
         <td
-          key="recipesQuantity"
-          class="content-center py-2 pl-2"
-        >
-          <menus-table-recipes-quantity
-            :menu="menu"
-          />
-        </td>
-        <!-- recipes -->
-        <td
-          key="recipes"
-          class="content-center py-2 pl-2"
-        >
-          <menus-table-recipes
-            :menu="menu"
-          />
-        </td>
-        <!-- dots -->
-        <td
+          v-if="dots"
           class="content-center"
         >
           <dots-dropdown
@@ -80,8 +50,8 @@
               edit:true,
               del:true
             }"
-            @edit="editIngredient(menu)"
-            @del="deleteIngredient(menu)"
+            @edit="editIngredient(parsedElement)"
+            @del="deleteIngredient"
           />
         </td>
       </tr>
@@ -90,26 +60,30 @@
 </template>
 
 <script>
-import MenusTableRecipesQuantity from './menus-table-recipes-quantity';
-import MenusTableRecipes from './menus-table-recipes';
 
 export default {
-  components: {
-    MenusTableRecipesQuantity,
-    MenusTableRecipes,
-  },
-
   props: {
-    menus: { type: Array, required: true },
+    header: { type: Array, required: true },
+    body: { type: Array, required: true },
+    dots: { type: Boolean, required: true },
+    modelType: { type: String, required: true },
   },
-
   methods: {
     editIngredient(element) {
       this.$emit('edit', element);
     },
-    deleteIngredient(element) {
-      this.$emit('del', element);
+    deleteIngredient() {
+      this.$emit('del');
     },
+  },
+
+  computed: {
+    parsedElements() {
+      const parsedElements = this.body.map((element) => JSON.parse(element));
+
+      return parsedElements;
+    },
+
   },
 };
 
