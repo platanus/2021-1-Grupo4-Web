9ef40964d25f469bffe82c8ea8c03579b930574e
miortiz1 <miortiz1@uc.cl>
2021-05-25 19:25:40 -0400

    feat(frontend/recipe-modal): add modal to create recipe

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,6 +24,9 @@ import RecipeShow from '../components/recipes/show/recipe-show.vue';
 import CreateRecipes from '../components/recipes/new/create-recipes.vue';
 import MenusContainer from '../components/menus/index/menus-container.vue';
 import NewMenuContainer from '../components/menus/new/new-menu-container.vue';
+import addButtonComponent from '../components/recipes/new/add-button.vue';
+import SelectedIngredients from '../components/recipes/new/selected-ingredients.vue';
+import SearchIngredientList from '../components/recipes/new/search-ingredient-list.vue';
 
 import formatCurrency from '../filters/format-currency.js';
 
@@ -47,6 +50,9 @@ Vue.component('IngredientsContainer', IngredientsContainer);
 Vue.component('RecipesContainer', RecipesContainer);
 Vue.component('RecipeShow', RecipeShow);
 Vue.component('CreateRecipes', CreateRecipes);
+Vue.component('addButtonComponent', addButtonComponent);
+Vue.component('SelectedIngredients', SelectedIngredients);
+Vue.component('SearchIngredientList', SearchIngredientList);
 Vue.component('MenusContainer', MenusContainer);
 Vue.component('NewMenuContainer', NewMenuContainer);
 
