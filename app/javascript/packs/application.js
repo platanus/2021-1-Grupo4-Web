a17075b95ccc8b318609491baaedb6ad6e754ecb
aescobar4 <aescobar4@uc.cl>
2021-05-24 21:59:01 -0400

    fix(frontend/create-recipes): fix details

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,20 +18,15 @@ import Register from '../components/users/register.vue';
 import Login from '../components/users/login.vue';
 import LandingPage from '../components/landing-page.vue';
 
-import RecipeInfo from '../components/recipes/show/base-recipe-info.vue';
-import RecipeIngredients from '../components/recipes/show/base-recipe-ingredients.vue';
-import RecipeIngredient from '../components/recipes/show/base-recipe-ingredient.vue';
-import RecipeInstructions from '../components/recipes/show/base-recipe-instructions.vue';
-import InstructionStep from '../components/recipes/show/base-instruction-step.vue';
-import CreateRecipes from '../components/recipes/create/base-create-recipes-component.vue';
-import List from '../components/recipes/create/base-list-component.vue';
-import Container from '../components/recipes/create/base-container-component.vue';
-import TextBox from '../components/base/base-text-box-component';
-import DragAndDrop from '../components/base/base-drag-and-drop-component';
-
 import IngredientsContainer from '../components/ingredients/ingredients-container.vue';
 import RecipesContainer from '../components/recipes/index/recipes-container.vue';
 import RecipeShow from '../components/recipes/show/recipe-show.vue';
+import CreateRecipes from '../components/recipes/create/create-recipes-component.vue';
+import List from '../components/recipes/create/list-component.vue';
+import Container from '../components/recipes/create/container-component.vue';
+import TextBox from '../components/base/base-text-box';
+import DragAndDrop from '../components/base/drag-and-drop';
+
 import MenusContainer from '../components/menus/index/menus-container.vue';
 import NewMenuContainer from '../components/menus/new/new-menu-container.vue';
 
@@ -53,11 +48,6 @@ Vue.component('LandingPage', LandingPage);
 Vue.component('IngredientsContainer', IngredientsContainer);
 Vue.component('RecipesContainer', RecipesContainer);
 Vue.component('RecipeShow', RecipeShow);
-Vue.component('RecipeInfo', RecipeInfo);
-Vue.component('RecipeIngredients', RecipeIngredients);
-Vue.component('RecipeIngredient', RecipeIngredient);
-Vue.component('RecipeInstructions', RecipeInstructions);
-Vue.component('InstructionStep', InstructionStep);
 Vue.component('CreateRecipes', CreateRecipes);
 Vue.component('List', List);
 Vue.component('Container', Container);
