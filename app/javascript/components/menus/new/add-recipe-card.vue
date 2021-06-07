2875c040f9389504dba5c4d2c7b65912fe5384b6
ValentinaAlvarezG <vjalvarez@uc.cl>
2021-06-07 11:54:04 -0400

    feat(new-menu): add new menu view with menu name form, search bar and recipes to add

---
+++ app/javascript/components/menus/new/add-recipe-card.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="flex items-start justify-between w-full h-20 border border-gray-500 bg-gray-50 flex-none flex-grow-0">
+    <!-- Image -->
+    <img
+      svg-inline
+      src="../../../../assets/images/recipe.jpg"
+      class="w-20 h-20 text-yellow-700"
+    >
+    <!-- Name and info -->
+    <div>
+      <p class="py-2 px-3 font-medium">
+        {{ name }}
+      </p>
+      <div>
+        <p class="py-2 px-3 text-xs inline-block">
+          {{ portions }} {{ $t('msg.recipes.portions') }}
+        </p>
+        <p class="py-2 px-3 text-xs inline-block">
+          {{ recipePrice | currency }}
+        </p>
+        <p class="py-2 px-3 text-xs inline-block">
+          {{ minutes }} {{ $t('msg.recipes.minutes') }}
+        </p>
+      </div>
+    </div>
+    <!-- button -->
+    <button
+      class="flex items-center py-1.5 px-4 w-auto h-9 bg-yellow-500 shadow-sm rounded-md flex-none flex-grow-0 my-auto mx-3"
+      @click="addRecipe()"
+    >
+      <div class="w-auto h-6 font-hind font-normal text-base text-white flex-none flex-grow-0">
+        {{ $t('msg.add') }}
+      </div>
+    </button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    name: { type: String, required: true },
+    portions: { type: Number, required: true },
+    minutes: { type: Number, required: true },
+    recipeIngredients: { type: Array, required: true },
+  },
+  methods: {
+    addRecipe() {
+      this.$emit('add');
+    },
+    getPriceOfSelectedIngredient(recipeIngredient) {
+      if (!recipeIngredient.ingredientQuantity) return 0;
+
+      return recipeIngredient.ingredientQuantity *
+      recipeIngredient.ingredient.price / recipeIngredient.ingredient.quantity;
+    },
+  },
+  computed: {
+    recipePrice() {
+      return this.recipeIngredients.reduce((recipePrice, recipeIngredient) =>
+        recipePrice + this.getPriceOfSelectedIngredient(recipeIngredient.attributes), 0);
+    },
+  },
+};
+</script>
--- app/javascript/components/menus/new/new-menu-container.vue
+++ app/javascript/components/menus/new/new-menu-container.vue
@@ -1,43 +1,141 @@
+
 <template>
   <div class="flex flex-col">
-    <div class="flex flex-row justify-between items-center h-8 order-0 flex-grow-0 mt-1 mb-4">
+    <!-- Title -->
+    <div class="flex items-center h-8 order-0 flex-grow-0 mt-1">
       <div class="text-4xl order-0 flex-grow-0">
         {{ $t('msg.menus.create') }}
       </div>
     </div>
-    <div class="flex">
-      <!-- creador menu -->
-      <div class="flex flex-col items-start w-4/6 h-auto bg-white flex-grow-0 mr-5 p-10">
-        <information-menu />
-        <div class="min-w-min font-hind font-normal text-base text-black flex-grow-0 mb-5">
-          {{ $t('msg.menus.addRecipes') }}
+    <div class="flex flex-col py-8 px-6 w-auto h-auto bg-gray-50 flex-grow-0 my-4">
+      <!-- Menu Name -->
+      <div class="relative w-2/5 ml-4">
+        <div class="text-gray-600 text-sm absolute bg-gray-50 px-1 left-2 -top-2">
+          {{ $t('msg.recipes.name') }}
         </div>
-        <search-recipes />
-        <available-recipes />
+        <input
+          class="w-full h-16 bg-gray-50 border border-gray-600 box-border rounded-md flex-none flex-grow-0 px-5"
+          v-model="menuName"
+        >
       </div>
-      <!-- recetas seleccionadas -->
-      <div class="flex flex-col items-start w-2/6 h-auto bg-white flex-grow-0 py-4 px-3">
-        <div class="w-auto h-10 flex-grow-0 py-2 px-3 mb-2 font-hind font-bold text-base text-black">
-          {{ $t('msg.menus.addRecipes') }}
+      <!-- Recipes -->
+      <div class="flex justify-between mb-8">
+        <div class="w-1/2 p-4">
+          <div class="flex flex-col">
+            <!-- search bar -->
+            <div class="relative text-yellow-700 my-4">
+              <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+                <img
+                  svg-inline
+                  src="../../../../assets/images/magnifyng-glass-svg.svg"
+                  class="w-6 h-6 text-yellow-700"
+                >
+              </span>
+              <input
+                class="flex py-2 px-12 w-96 h-16 bg-gray-50 border-2 border-gray-600 rounded self-stretch flex-grow-0 focus:outline-none"
+                :placeholder="$t('msg.recipes.search')"
+                autocomplete="off"
+              >
+            </div>
+            <!-- available recipes -->
+            <div class="flex flex-col bg-gray-200 overflow-scroll">
+              <add-recipe-card
+                v-for="recipe in recipes"
+                :key="recipe.id"
+                :name="recipe.name"
+                :portions="recipe.portions"
+                :minutes="recipe.cookMinutes"
+                :recipe-ingredients="recipe.recipeIngredients.data"
+                @add="addRecipe(recipe)"
+              >
+                {{ recipe.name }}
+              </add-recipe-card>
+            </div>
+          </div>
         </div>
-        <selected-recipes />
+        <!-- selected recipes -->
+        <!-- <div class="w-1/2 p-4">
+          <div class="flex flex-col self-stretch flex-grow bg-gray-50">
+            <div class="flex h-6 bg-gray-50 font-sans font-medium text-base text-black self-stretch mb-3">
+              {{ $t('msg.recipes.selectedIngredients') }}
+            </div>
+            <div
+              class="flex flex-col bg-gray-200 overflow-scroll"
+              v-if="recipeIngredients.length > 0"
+            >
+              <div
+                v-for="(recipeIngredient, idx) in recipeIngredients"
+                :key="recipeIngredient.id"
+              >
+                <selected-recipes-card
+                  :recipe-ingredient-idx="idx"
+                  :recipe-ingredient-attrs="recipeIngredient.attributes"
+                  @delete-ingredient="deleteIngredient"
+                  @increase-quantity="increaseQuantity"
+                  @decrease-quantity="decreaseQuantity"
+                >
+                  {{ recipeIngredient.attributes.ingredient.name }}
+                </selected-recipes-card>
+              </div>
+            </div>
+            <div
+              class="flex h-6 bg-gray-50 font-sans font-light text-base text-black self-stretch mb-3"
+              v-else
+            >
+              {{ $t('msg.recipes.noIngredients') }}
+            </div>
+            <div class="flex justify-end py-4 px-2 bg-gray-200 border border-gray-300 box-border self-stretch">
+              <div class="w-auto h-6 font-bold text-base text-black mx-2">
+                {{ $t('msg.recipes.recipePrice') }} $ {{ recipePrice }}
+              </div>
+            </div>
+          </div>
+        </div> -->
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import InformationMenu from './information-menu.vue';
-import SearchRecipes from './search-recipes.vue';
-import AvailableRecipes from './available-recipes/available-recipes.vue';
-import SelectedRecipes from './selected-recipes/selected-recipes.vue';
+import { getRecipes } from '../../../api/recipes.js';
+import SelectedRecipesCard from './selected-recipes-card.vue';
+import AddRecipeCard from './add-recipe-card';
 
 export default {
   components: {
-    InformationMenu,
-    SearchRecipes,
-    AvailableRecipes,
-    SelectedRecipes,
+    SelectedRecipesCard,
+    AddRecipeCard,
+  },
+
+  data() {
+    return {
+      recipes: [],
+      error: '',
+      query: '',
+      menuName: '',
+    };
+  },
+
+  computed: {
+  },
+
+  async created() {
+    try {
+      const response = await getRecipes();
+      this.recipes = response.data.data.map((element) => ({
+        id: element.id,
+        ...element.attributes,
+      }));
+      this.error = '';
+    } catch (error) {
+      this.error = error;
+    }
+  },
+
+  methods: {
+    addRecipe(recipe) {
+      console.log('add-recipe', recipe);
+    },
   },
 };
 </script>
