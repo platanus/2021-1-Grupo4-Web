617bc12eb7922af5a3394dc3dab3eecbf42165bd
Andrés Rodríguez <arodriguez14@uc.cl>
2021-06-28 11:42:01 -0400

    fix(form-ingredients): add alerts in wrong atrributes

--- app/javascript/locales/es.js
+++ app/javascript/locales/es.js
@@ -53,6 +53,8 @@ export default {
       defaultUnit: 'Unidad por defecto',
       alternativeUnit: 'Unidades alternativas',
       addUnit: 'Agregar Unidad',
+      msjAlert: 'Debe ingresar el nombre, la cantidad y la unidad',
+      msjMinQuantity: 'La cantidad mínima debe ser mayor o igual a 1',
     },
 
     recipes: {
