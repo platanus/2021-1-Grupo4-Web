c203a869c0849b52b2291e014b15e6889b85f517
ValentinaAlvarezG <vjalvarez@uc.cl>
2021-06-28 18:48:30 -0400

    style(see-moremodal): fix style in see more recipes of menu modal

--- app/javascript/components/menus/index/menus-table-recipes.vue
+++ app/javascript/components/menus/index/menus-table-recipes.vue
@@ -21,19 +21,11 @@
       :ok-button-present="false"
       :cancel-button-label="$t('msg.cancel')"
     >
-      <p class="text-lg">
-        {{ $t('msg.recipes.title') }}:
-      </p>
-      <div
-        v-for="recipe in menu.menuRecipes.data"
-        :key="recipe.attributes.recipe.id"
-        class="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2"
-      >
-        <span class="bg-gray-600 h-2 w-2 m-2 rounded-full" />
-        <div class="flex-grow text-sm px-2">
-          {{ recipe.attributes.recipeQuantity }} {{ recipe.attributes.recipe.name }}
-        </div>
-      </div>
+      <p>{{ $t('msg.menus.menuRecipes') }}</p>
+      <base-one-column-table
+        :header="$t('msg.recipes.title')"
+        :rows="allMenuRecipes"
+      />
     </base-modal>
   </div>
 </template>
@@ -58,6 +50,10 @@ export default {
     },
     seeMore() {
       return this.menu.menuRecipes.data.length > this.firstRecipes.length;
+    },
+    allMenuRecipes() {
+      return this.menu.menuRecipes.data.map((recipe) =>
+        `${recipe.attributes.recipeQuantity} ${recipe.attributes.recipe.name}`);
     },
   },
 
