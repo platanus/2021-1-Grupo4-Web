155cff58b293da0253819d0e81911af618856a8a
sofiadelano <smdelano@uc.cl>
2021-07-01 17:02:52 -0400

    fix(menus/table): change table and fix problem when zooming

--- app/javascript/components/menus/index/menus-container.vue
+++ app/javascript/components/menus/index/menus-container.vue
@@ -47,11 +47,12 @@
         >
           {{ $t('msg.noElements') }} {{ $t('msg.menus.title').toLowerCase() }}
         </p>
-        <menus-table
-          v-else
+        <div v-else class="flex w-full 2xl:justify-center items-center overflow-auto">
+          <menus-table
           :menus="filterMenus"
           @del="toggleDelModal"
         />
+        </div>
       </div>
     </div>
 
