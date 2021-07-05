682c7a06206785735f0d57bd991927b09fe83ce4
pipe <fncabrera@uc.cl>
2021-07-05 01:46:50 -0400

    style(side-bar): sidebar style changed and left margin of containers in each view

--- app/javascript/components/layout/side-navbar-item.vue
+++ app/javascript/components/layout/side-navbar-item.vue
@@ -1,21 +1,29 @@
 <template>
-  <div class="flex">
+  <div
+    class="flex h-20 w-20"
+    :class="active ? 'bg-yellow-500' : 'bg-gray-800 hover:bg-gray-600'"
+    @mouseover="mouseOver"
+    @mouseleave="notMouseOver"
+  >
     <button
       @click="clickButton"
-      class="focus:outline-none"
-      :class="active ? 'bg-yellow-500' : 'bg-gray-800 hover:bg-gray-600'"
+      class="focus:outline-none w-full"
     >
-      <div class="flex flex-row items-center pt-1 pl-10 pb-1 pr-2 static w-64 h-16 left-0">
-        <slot />
-        <div class="static w-16 pl-2 items-center left-16 not-italic font-normal text-xl text-white">
-          {{ label }}
-        </div>
+      <div class="flex flex-row justify-center p-2">
+        <img
+          class="h-12 w-12"
+          svg-inline
+          :src="require(`./../../../assets/images/${imgSrc}`)"
+        >
       </div>
     </button>
     <div
-      v-if="active"
-      class="bg-yellow-500 transform rotate-45 h-10 w-10 -ml-6 m-auto"
-    />
+      v-if="showLabel"
+      class="flex items-center z-50 pl-4 -mr-36 w-36 font-bold text-xl text-white"
+      :class="active ? 'bg-yellow-500' : 'bg-gray-600'"
+    >
+      {{ label }}
+    </div>
   </div>
 </template>
 
@@ -24,10 +32,22 @@ export default {
   props: {
     label: { type: String, required: true },
     active: { type: Boolean, required: true },
+    imgSrc: { type: String, required: true },
+  },
+  data() {
+    return {
+      showLabel: false,
+    };
   },
   methods: {
     clickButton() {
       this.$emit('click', this.label);
+    },
+    mouseOver() {
+      this.showLabel = true;
+    },
+    notMouseOver() {
+      this.showLabel = false;
     },
   },
 };
