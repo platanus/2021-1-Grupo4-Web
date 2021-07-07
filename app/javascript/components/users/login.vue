cc7f795564047be9648e555df4137fdd67e4a09b
sofiadelano <smdelano@uc.cl>
2021-07-07 19:10:42 -0400

    refactor(login): change style

--- app/javascript/components/users/login.vue
+++ app/javascript/components/users/login.vue
@@ -1,67 +1,70 @@
 <template>
-  <div class="m-auto max-w-xs">
-    <form
-      @submit.prevent="login"
-      method="post"
-      class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-    >
-      <h2 class="pb-5 text-4xl text-center">
-        {{ $t('msg.users.login') }}
+  <div class="flex flex-col items-center bg-gray-200">
+    <!-- mensaje bienvenida -->
+    <div class="flex flex-col items-center flex-none flex-grow-0 mb-4">
+      <img
+        class="h-16 w-16 pt-4 m-auto text-white"
+        svg-inline
+        src="../../../assets/images/chef-svg.svg"
+      >
+      <h1 class="font-bold text-3xl text-gray-700 flex-none flex-grow-0 mb-1">
+        {{ $t(`msg.welcomeTitle`) }}
+      </h1>
+      <h2 class="">
+        {{ $t(`msg.welcomeSubtitle`) }}
       </h2>
-      <div class="mb-4">
-        <label
-          class="block text-gray-700 text-sm font-bold mb-2"
-          for="email"
-        >
-          {{ $t('msg.users.labelEmail') }}
-        </label>
-        <input
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="email"
-          type="email"
-          :placeholder="$t('msg.users.placeholderEmail')"
-          v-model="form.userEmail"
-        >
-        <p
-          v-if="errors.email"
-          class="mt-2 ml-1 text-xs text-red-400"
-        >
-          {{ $t(`msg.users.${errors.email}`) }}
-        </p>
-      </div>
-      <div class="mb-4">
-        <label
-          class="block text-gray-700 text-sm font-bold mb-2"
-          for="password"
-        >
-          {{ $t('msg.users.labelPassword') }}
-        </label>
-        <input
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="password"
-          type="password"
-          placeholder="******************"
-          v-model="form.userPassword"
-        >
-        <p
-          v-if="errors.password"
-          class="mt-2 ml-1 text-xs text-red-400"
-        >
-          {{ $t(`msg.users.${errors.password}`) }}
-        </p>
-      </div>
-      <div class="flex items-center justify-between">
-        <base-button
-          :elements="{ placeholder: $t('msg.users.login'),
-                       color: 'bg-green-500 hover:bg-green-700 text-white' }"
-        />
-      </div>
-      <div class="text-gray-700 text-center text-sm font-bold pt-2">
-        <a href="/users/sign_up">
-          {{ $t('msg.users.notHaveAccount') }}
-        </a>
-      </div>
-    </form>
+    </div>
+    <div class="flex flex-col justify-center items-center p-12 bg-white rounded flex-none flex-grow-0 w-auto">
+      <form
+        @submit.prevent="login"
+        method="post"
+      >
+        <h2 class="font-bold text-2xl text-black flex-none self-stretch flex-grow-0 mb-8">
+          {{ $t('msg.users.login') }}
+        </h2>
+        <div class="w-full mb-8">
+          <input
+            class="flex items-start w-full py-5 px-4 bg-white border border-solid border-gray-50 rounded-md flex-none flex-grow-0"
+            id="email"
+            type="email"
+            :placeholder="$t('msg.users.placeholderEmail')"
+            v-model="form.userEmail"
+          >
+          <p
+            v-if="errors.email"
+            class="mt-2 ml-1 text-xs text-red-400"
+          >
+            {{ $t(`msg.users.${errors.email}`) }}
+          </p>
+        </div>
+        <div class="mb-8 w-full">
+          <input
+            class="flex items-start w-full py-5 px-4 bg-white border border-solid border-gray-50 rounded-md flex-none flex-grow-0"
+            id="password"
+            type="password"
+            placeholder="******************"
+            v-model="form.userPassword"
+          >
+          <p
+            v-if="errors.password"
+            class="mt-2 ml-1 text-xs text-red-400"
+          >
+            {{ $t(`msg.users.${errors.password}`) }}
+          </p>
+        </div>
+        <div class=" w-auto flex justify-between items-end flex-none self-stretch flex-grow-0 mb-8">
+          <div class="font-normal text-base underline text-yellow-500 flex-none flex-grow-0 mb-1 mr-8">
+            <a href="/users/sign_up">
+              {{ $t('msg.users.notHaveAccount') }}
+            </a>
+          </div>
+          <base-button
+            :elements="{ placeholder: $t('msg.users.login'),
+                         color: 'bg-green-500 hover:bg-green-700 text-white' }"
+          />
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
