c56f49687444a140eb08445eca7d32a13001bb91
aescobar4 <aescobar4@uc.cl>
2021-07-12 11:46:16 -0400

    refactor(ingredientsRecipesDetails): refactor of recipe index

--- app/javascript/components/recipes/index/recipes-list/recipes-item.vue
+++ app/javascript/components/recipes/index/recipes-list/recipes-item.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex flex-row">
-    <div class="flex flex-row w-10/12 justify-between items-center p-6 static h-24 bg-white border-2 border-solid border-gray-400 box-border my-2 flex-none flex-grow-0 self-stretch">
+  <div class="flex flex-row divide-x-2 divide-gray-400 border-2 border-solid border-gray-400 box-border my-2">
+    <div class="flex flex-row w-10/12 justify-between items-center p-6 static h-24 bg-white flex-none flex-grow-0 self-stretch">
       <recipes-item-info
         :recipe-name="recipe.name"
         :recipe-portions="recipe.portions"
@@ -11,20 +11,35 @@
         :recipe-price="recipePrice"
       />
     </div>
-
-    <div class="flex flex-col justify-center items-center w-full">
-      <a
-        class=" flex justify-center items-center cursor-pointer h-12 mb-2 underline text-xl focus:outline-none"
-        :href="`/recipes/${recipe.id}/edit`"
-      >
-        {{ $t('msg.recipes.edit') }}
-      </a>
-      <button
-        class="flex justify-center cursor-pointer h-12 underline text-xl text-red-600 focus:outline-none"
-        @click="toggleDelModal"
-      >
-        {{ $t('msg.recipes.delete') }}
-      </button>
+    <div class="flex w-full">
+      <div class="flex flex-col justify-center items-center w-full">
+        <a
+          class=" flex justify-center items-center cursor-pointer h-12 underline text-xl focus:outline-none"
+          :href="`/recipes/${recipe.id}/edit`"
+        >
+          <span>
+            <img
+              svg-inline
+              src="../../../../../assets/images/pencil-svg.svg"
+              class="text-black h-6 w-6"
+            >
+          </span>
+          {{ $t('msg.recipes.edit') }}
+        </a>
+        <button
+          class="flex justify-center cursor-pointer h-12 underline text-xl text-red-600 focus:outline-none"
+          @click="toggleDelModal"
+        >
+          <span>
+            <img
+              svg-inline
+              src="../../../../../assets/images/red-trashcan-svg.svg"
+              class="stroke-current text-red-600 h-6 w-6"
+            >
+          </span>
+          {{ $t('msg.recipes.delete') }}
+        </button>
+      </div>
     </div>
 
     <!--DeleteModal-->
