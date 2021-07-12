ad60757ed6d471c8795365658fe2045a27334d31
aescobar4 <aescobar4@uc.cl>
2021-07-12 11:07:45 -0400

    feat(indexRecipes): edit and delete recipe from index

--- app/javascript/components/recipes/index/recipes-list/recipes-item.vue
+++ app/javascript/components/recipes/index/recipes-list/recipes-item.vue
@@ -1,18 +1,67 @@
 <template>
-  <div class="flex flex-row justify-between items-center p-6 static h-24 bg-white border-2 border-solid border-gray-400 box-border my-2 flex-none flex-grow-0 self-stretch">
-    <recipes-item-info
-      :recipe-name="recipe.name"
-      :recipe-portions="recipe.portions"
-      :recipe-minutes="recipe.cookMinutes"
-    />
-    <recipes-item-price
-      :id-recipe="recipe.id"
-      :recipe-price="recipePrice"
-    />
+  <div class="flex flex-row">
+    <div class="flex flex-row w-10/12 justify-between items-center p-6 static h-24 bg-white border-2 border-solid border-gray-400 box-border my-2 flex-none flex-grow-0 self-stretch">
+      <recipes-item-info
+        :recipe-name="recipe.name"
+        :recipe-portions="recipe.portions"
+        :recipe-minutes="recipe.cookMinutes"
+      />
+      <recipes-item-price
+        :id-recipe="recipe.id"
+        :recipe-price="recipePrice"
+      />
+    </div>
+
+    <div class="flex flex-col justify-center items-center w-full">
+      <a
+        class=" flex justify-center items-center cursor-pointer h-12 mb-2 underline text-xl focus:outline-none"
+        :href="`/recipes/${recipe.id}/edit`"
+      >
+        {{ $t('msg.recipes.edit') }}
+      </a>
+      <button
+        class="flex justify-center cursor-pointer h-12 underline text-xl text-red-600 focus:outline-none"
+        @click="toggleDelModal"
+      >
+        {{ $t('msg.recipes.delete') }}
+      </button>
+    </div>
+
+    <!--DeleteModal-->
+    <base-modal
+      @ok="deleteRecipe"
+      @cancel="toggleDelModal"
+      v-if="showingDel"
+      :title="$t('msg.recipes.delete')"
+      :ok-button-label="$t('msg.yesDelete')"
+      :cancel-button-label="$t('msg.cancel')"
+    >
+      <!-- Critical associations -->
+      <span
+        class="flex my-auto w-8 h-8 pl-2"
+        v-if="loadingAssociations"
+      >
+        <base-spinner />
+      </span>
+      <div v-else>
+        <div v-if="criticalAssociations.length > 0">
+          <p>{{ $t('msg.recipes.associationWarning') }}</p>
+          <base-one-column-table
+            :header="$t('msg.menus.title')"
+            :rows="criticalAssociations"
+          />
+        </div>
+        <!-- Delete msg -->
+        <p class="mt-4">
+          {{ $t('msg.recipes.deleteMsg') }}
+        </p>
+      </div>
+    </base-modal>
   </div>
 </template>
 
 <script>
+import { deleteRecipe, getCriticalAssociations } from '../../../../api/recipes.js';
 import RecipesItemInfo from './recipes-item-info';
 import RecipesItemPrice from './recipes-item-price';
 import { getPriceOfSelectedIngredient } from '../../../../utils/recipeUtils.js';
@@ -27,9 +76,45 @@ export default {
   props: {
     recipe: { type: Object, required: true },
   },
+  data() {
+    return {
+      showingDel: false,
+      loading: false,
+      loadingAssociations: false,
+    };
+  },
   methods: {
     ingredientFinalPrice(quantity, price) {
       return (quantity * price);
+    },
+    toggleDelModal() {
+      this.showingDel = !this.showingDel;
+      if (this.showingDel) {
+        this.getIngredientAssociations(this.recipe.id);
+      }
+    },
+    async getIngredientAssociations(recipeId) {
+      this.loadingAssociations = true;
+      try {
+        const response = await getCriticalAssociations(recipeId);
+        this.criticalAssociations = response.data.menus.map((element) => element.name);
+      } catch (error) {
+        this.error = error;
+      } finally {
+        this.loadingAssociations = false;
+      }
+    },
+    async deleteRecipe() {
+      this.loading = true;
+      try {
+        await deleteRecipe(this.recipe.id);
+        window.location.href = '/recipes';
+        this.error = '';
+      } catch (error) {
+        this.error = error;
+      } finally {
+        this.loading = false;
+      }
     },
   },
   computed: {
