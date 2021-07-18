7d92163d6c7ec1a793111483f20fe8d621e83f9d
sofiadelano <smdelano@uc.cl>
2021-07-17 21:04:58 -0400

    fix(menus): change input type of portions to number

--- app/javascript/components/menus/edit/edit-menu-container.vue
+++ app/javascript/components/menus/edit/edit-menu-container.vue
@@ -60,6 +60,7 @@
           <input
             class="w-full h-16 bg-gray-50 border border-gray-600 box-border rounded-md flex-none flex-grow-0 px-5"
             v-model="menuPortions"
+            type="number"
           >
           <p
             v-if="errors.portions"
