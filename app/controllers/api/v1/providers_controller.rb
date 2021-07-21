6dc4d4896a2e17429fb29664dd597c2844a3ad1d
cacosandon <jiossandon@uc.cl>
2021-07-20 22:39:11 -0400

    feat(providers): sort index and unshift new one

--- app/controllers/api/v1/providers_controller.rb
+++ app/controllers/api/v1/providers_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::ProvidersController < Api::V1::BaseController
   acts_as_token_authentication_handler_for User, fallback: :exception
 
   def index
-    respond_with providers
+    respond_with providers.order(:name)
   end
 
   def show
